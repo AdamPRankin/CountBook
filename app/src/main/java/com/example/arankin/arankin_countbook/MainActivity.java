821dc01a5e8c3d60e970c6fc0d24c47b91f7b9fd
Adam Rankin <arankin@cs.ualberta.ca>
2017-09-11 14:57:10 -0600

    added button that opens new counter activity

--- app/src/main/java/com/example/arankin/arankin_countbook/MainActivity.java
+++ app/src/main/java/com/example/arankin/arankin_countbook/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.arankin.arankin_countbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button counterButton = (Button) findViewById(R.id.newCounterButton);
+        counterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent counterPage = new Intent(MainActivity.this, AddCounterActivity.class);
+                startActivity(counterPage);
+            }
+        });
     }
 }
